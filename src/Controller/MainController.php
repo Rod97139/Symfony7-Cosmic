b21041a9b2f39cd33c0ef621428cef2714f517e2
Rod97139 <fayaflame@gmail.com>
2024-10-05 17:40:36 +0200

    twig templates

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,15 +2,26 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-class MainController
+class MainController extends AbstractController
 {
     #[Route('/', name: 'homepage')]
     public function homepage(): Response
     {
-        return new Response('Welcome to the homepage');
+        $starshipCount = 457;
+        $myShip = [
+        'name' => 'USS LeafyCruiser (NCC-0001)',
+        'class' => 'Garden',
+        'captain' => 'Jean-Luc Pickles',
+        'status' => 'under construction',
+        ];
+        return $this->render('main/homepage.html.twig', [
+            'numberOfStarships' => $starshipCount,
+            'myShip' => $myShip,
+        ]);
     }
 
 }
